df5dd4c05bfed8d3e797638a25b5672ea775deb0
Renan Estery <renan.estery@compasso.com.br>
2023-12-27 18:11:07 -0300

    feature: created components

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,26 +1,29 @@
 import { PencilLine } from 'phosphor-react';
+import { Avatar } from './Avatar';
+
 import styles from './Sidebar.module.css';
+
 export function Sidebar() {
   return (
-    <sidebar className={styles.sidebar}>
+    <aside className={styles.sidebar}>
       <img
         className={styles.cover}
-        src="https://images.unsplash.com/photo-1604964432806-254d07c11f32?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        alt=""
+        src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
       />
+
       <div className={styles.profile}>
-        <img
-          className={styles.avatar}
-          src="https://github.com/RenanEstery20.png"
-        />
+        <Avatar src="https://github.com/RenanEstery20.png" />
+
         <strong>Renan Estery</strong>
         <span>Web Developer</span>
       </div>
+
       <footer>
         <a href="#">
-          <PencilLine size={20} /> Editar seu perfil
+          <PencilLine size={20} />
+          Editar seu perfil
         </a>
       </footer>
-    </sidebar>
+    </aside>
   );
 }
